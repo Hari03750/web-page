fb20f2616e107c07eee0d3b67c7725bad3d704a6
Hari03750 <hariharan2004b@gmail.com>
2025-05-29 21:12:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
-  document.querySelector(".nav ul").classList.toggle("active");
+const menuBtn = document.getElementById("menu-btn");
+const navList = document.querySelector("nav ul");
+
+menuBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
 });
 
